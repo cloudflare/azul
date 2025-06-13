83fb9deab7e8ef2f8ceadb7c23cdf102fa137998
Luke Valenta <lvalenta@cloudflare.com>
2025-06-13 14:58:17 -0400

    bugfix: strip suffix from batcher object name to get log name

--- crates/ct_worker/src/batcher_do.rs
+++ crates/ct_worker/src/batcher_do.rs
@@ -12,19 +12,22 @@ struct Batcher(GenericBatcher<StaticCTPendingLogEntry>);
 #[durable_object]
 impl DurableObject for Batcher {
     fn new(state: State, env: Env) -> Self {
-        let (_, name) = get_durable_object_name(state).unwrap();
-        let params = &CONFIG.logs[&name];
-        let kv = load_cache_kv(&env, &name).unwrap();
+        let (_, object_name) = get_durable_object_name(state).unwrap();
+        // Get the log name from the batcher name (see 'get_durable_object_stub'
+        // for how the batcher name is derived).
+        let name = object_name.rsplit_once('_').unwrap().0;
+        let params = &CONFIG.logs[name];
+        let kv = load_cache_kv(&env, name).unwrap();
         let sequencer = get_durable_object_stub(
             &env,
-            &name,
+            name,
             None,
             "SEQUENCER",
             params.location_hint.as_deref(),
         )
         .unwrap();
         let config = BatcherConfig {
-            name,
+            name: name.to_string(),
             max_batch_entries: params.max_batch_entries,
             batch_timeout_millis: params.batch_timeout_millis,
         };
