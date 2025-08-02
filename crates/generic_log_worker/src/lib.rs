fc116904e7e984997812f14a741038ee235e7345
Luke Valenta <lvalenta@cloudflare.com>
2025-08-01 20:37:26 -0400

    Double-check that R2 object uploads are successful
    As a measure to prevent #84, immediately confirm that we can fetch the
    correct objects from R2 after they are uploaded.
    
    I was having trouble getting the sentry integration working, so in the
    meantime update the Cleaner to just check that each full level-0 tile
    exists. This will stop progress indefinitely to allow us to detect if
    more tiles go missing.

--- crates/generic_log_worker/src/lib.rs
+++ crates/generic_log_worker/src/lib.rs
@@ -538,7 +538,7 @@ impl ObjectBackend for ObjectBucket {
             .as_ref()
             .inspect(|&m| m.upload_size_bytes.observe(value.len().as_f64()));
         self.bucket
-            .put(key.as_ref(), value)
+            .put(key.as_ref(), value.clone())
             .http_metadata(metadata)
             .execute()
             .await
@@ -547,6 +547,23 @@ impl ObjectBackend for ObjectBucket {
                     .as_ref()
                     .inspect(|&m| m.errors.with_label_values(&["put"]).inc());
             })?;
+
+        // SAFETY: immediately fetch the uploaded object to make sure that it was persisted.
+        // TODO: sentry reporting
+        if let Some(fetched) = self.fetch(key.as_ref()).await? {
+            if fetched != value {
+                return Err(Error::RustError(format!(
+                    "tile persisted with wrong contents: {}",
+                    key.as_ref()
+                )));
+            }
+        } else {
+            return Err(Error::RustError(format!(
+                "tile not persisted: {}",
+                key.as_ref()
+            )));
+        }
+
         self.metrics.as_ref().inspect(|&m| {
             m.duration
                 .with_label_values(&["put"])
