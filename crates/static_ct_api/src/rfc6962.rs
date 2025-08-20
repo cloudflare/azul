cloudflare/azul

commit fb4f44d30ab555650f590e9660004de61e5b9c4f

Clean up parsing in static_ct_api (#46)
* Use '&[u8]' (which implements Read) instead of Cursor where applicable
* Clean up SCT extensions parsing
* Clean up chain_fingerprints parsing
* Add enum for EntryType (X509Cert or Precert) and use where applicable
* Create PrecertData wrapper around fields that are only present in
  precerts, put an optional in StaticCTPendingLogEntry. The presence of
  the optional indicates whether the entry is a cert or precert.
* Make validate_chain return a StaticCTPendingLogEntry and remove
  ValidatedChain struct. And there's no need for validate_chain to
  return the raw issuers since the caller already has the raw chain.
* Use tlog_tiles::LookupKey instead of static_ct_api::LookupKey

crates/static_ct_api/src/rfc6962.rs
@@ -20,8 +20,7 @@
 //! - [cert_checker.go](https://github.com/google/certificate-transparency-go/blob/74d106d3a25205b16d571354c64147c5f1f7dbc1/trillian/ctfe/cert_checker.go)
 //! - [cert_checker_test.go](https://github.com/google/certificate-transparency-go/blob/74d106d3a25205b16d571354c64147c5f1f7dbc1/trillian/ctfe/cert_checker_test.go)
 
-use crate::StaticCTError;
-use crate::ValidatedChain;
+use crate::{PrecertData, StaticCTError, StaticCTPendingLogEntry};
 use der::{
     asn1::{Null, OctetString},
     oid::{
@@ -80,7 +79,7 @@ pub struct GetRootsResponse {
     pub certificates: Vec<Vec<u8>>,
 }
 
-/// Validates a certificate chain and returns a [`ValidatedChain`].
+/// Validates a certificate chain and returns a pending log entry.
 ///
 /// # Errors
 ///
@@ -93,7 +92,7 @@ pub fn validate_chain(
     not_after_end: Option<UnixTimestamp>,
     expect_precert: bool,
     require_server_auth_eku: bool,
-) -> Result<ValidatedChain, StaticCTError> {
+) -> Result<StaticCTPendingLogEntry, StaticCTError> {
     // First make sure the cert parses as X.509.
     let mut iter = raw_chain.iter();
     let leaf: Certificate = match iter.next() {
@@ -183,16 +182,12 @@ pub fn validate_chain(
         return Err(StaticCTError::NoPathToTrustedRoot { to_verify_issuer });
     }
 
-    // Return a [ValidatedChain] constructed from the chain.
-    let issuers = intermediates
-        .iter()
-        .map(der::Encode::to_der)
-        .collect::<Result<_, _>>()?;
-    let issuer_key_hash: [u8; 32];
-    let pre_certificate: Vec<u8>;
+    // Construct a pending log entry.
+    let precert_opt: Option<PrecertData>;
     let certificate: Vec<u8>;
     if is_precert {
         let mut pre_issuer: Option<&TbsCertificate> = None;
+        let issuer_key_hash: [u8; 32];
         if has_precert_signing_cert {
             pre_issuer = Some(&intermediates[0].tbs_certificate);
             if intermediates.len() < 2 {
@@ -214,20 +209,26 @@ pub fn validate_chain(
             )
             .into();
         }
-        pre_certificate = leaf.to_der()?;
+        let pre_certificate = leaf.to_der()?;
+        precert_opt = Some(PrecertData {
+            issuer_key_hash,
+            pre_certificate,
+        });
         certificate = build_precert_tbs(&leaf.tbs_certificate, pre_issuer)?;
     } else {
-        pre_certificate = Vec::new();
+        precert_opt = None;
         certificate = leaf.to_der()?;
-        issuer_key_hash = [0; 32];
     }
 
-    Ok(ValidatedChain {
+    let chain_fingerprints = raw_chain[1..]
+        .iter()
+        .map(|issuer| Sha256::digest(issuer).into())
+        .collect();
+
+    Ok(StaticCTPendingLogEntry {
         certificate,
-        is_precert,
-        issuer_key_hash,
-        issuers,
-        pre_certificate,
+        precert_opt,
+        chain_fingerprints,
     })
 }
 

crates/ct_worker/src/ctlog.rs
@@ -1189,7 +1189,7 @@ mod tests {
         Rng, RngCore, SeedableRng,
     };
     use signed_note::{Note, VerifierList};
-    use static_ct_api::{StandardEd25519CheckpointSigner, StaticCTCheckpointSigner};
+    use static_ct_api::{PrecertData, StandardEd25519CheckpointSigner, StaticCTCheckpointSigner};
     use static_ct_api::{StaticCTLogEntry, StaticCTPendingLogEntry};
     use std::cell::RefCell;
     use tlog_tiles::{Checkpoint, TlogTile};
@@ -1234,7 +1234,8 @@ mod tests {
                 let certificate = (i * 3000 + k).to_be_bytes().to_vec();
                 let leaf = StaticCTPendingLogEntry {
                     certificate,
-                    ..Default::default()
+                    precert_opt: None,
+                    chain_fingerprints: vec![[0; 32], [1; 32], [2; 32]],
                 };
                 add_leaf_to_pool(&mut log.pool_state, &log.cache, &log.config, leaf);
             }
@@ -2204,21 +2205,22 @@ mod tests {
             let mut rng = SmallRng::seed_from_u64(seed);
             let mut certificate = vec![0; rng.gen_range(8..12)];
             rng.fill(&mut certificate[..]);
-            let mut pre_certificate: Vec<u8>;
-            let mut issuer_key_hash = [0; 32];
-            if is_precert {
-                pre_certificate = vec![0; rng.gen_range(1..5)];
-                rng.fill(&mut pre_certificate[..]);
+            let precert_opt: Option<PrecertData> = if is_precert {
+                let mut issuer_key_hash = [0; 32];
                 rng.fill(&mut issuer_key_hash);
+                let mut pre_certificate = vec![0; rng.gen_range(1..5)];
+                rng.fill(&mut pre_certificate[..]);
+                Some(PrecertData {
+                    issuer_key_hash,
+                    pre_certificate,
+                })
             } else {
-                pre_certificate = Vec::new();
-            }
+                None
+            };
             let issuers = CHAINS[rng.gen_range(0..CHAINS.len())];
             let leaf = StaticCTPendingLogEntry {
                 certificate,
-                pre_certificate,
-                is_precert,
-                issuer_key_hash,
+                precert_opt,
                 chain_fingerprints: issuers.iter().map(|&x| Sha256::digest(x).into()).collect(),
             };
 
@@ -2314,12 +2316,9 @@ mod tests {
                     );
 
                     ensure!(!entry.inner.certificate.is_empty());
-                    if entry.inner.is_precert {
-                        ensure!(!entry.inner.pre_certificate.is_empty());
-                        ensure!(entry.inner.issuer_key_hash != [0; 32]);
-                    } else {
-                        ensure!(entry.inner.pre_certificate.is_empty());
-                        ensure!(entry.inner.issuer_key_hash == [0; 32]);
+                    if let Some(precert_data) = entry.inner.precert_opt {
+                        ensure!(!precert_data.pre_certificate.is_empty());
+                        ensure!(precert_data.issuer_key_hash != [0; 32]);
                     }
 
                     for fp in entry.inner.chain_fingerprints {

crates/ct_worker/src/frontend_worker.rs
@@ -13,11 +13,7 @@ use log::{debug, warn, Level};
 use p256::pkcs8::EncodePublicKey;
 use serde::Serialize;
 use serde_with::{base64::Base64, serde_as};
-use sha2::{Digest, Sha256};
-use static_ct_api::{
-    AddChainRequest, GetRootsResponse, PendingLogEntryTrait, StaticCTLogEntry,
-    StaticCTPendingLogEntry,
-};
+use static_ct_api::{AddChainRequest, GetRootsResponse, PendingLogEntryTrait, StaticCTLogEntry};
 use std::str::FromStr;
 #[allow(clippy::wildcard_imports)]
 use worker::*;
@@ -164,7 +160,7 @@ async fn add_chain_or_pre_chain(
     let req: AddChainRequest = req.json().await?;
 
     // Temporal interval dates prior to the Unix epoch are treated as the Unix epoch.
-    let chain = match static_ct_api::validate_chain(
+    let pending_entry = match static_ct_api::validate_chain(
         &req.chain,
         &ROOTS,
         Some(
@@ -187,19 +183,6 @@ async fn add_chain_or_pre_chain(
 
     // Retrieve the sequenced entry for this pending log entry by first checking the
     // deduplication cache and then sending a request to the DO to sequence the entry.
-
-    // Convert chain to a pending log entry.
-    let pending_entry = StaticCTPendingLogEntry {
-        certificate: chain.certificate,
-        is_precert: chain.is_precert,
-        issuer_key_hash: chain.issuer_key_hash,
-        chain_fingerprints: chain
-            .issuers
-            .iter()
-            .map(|issuer| Sha256::digest(issuer).into())
-            .collect(),
-        pre_certificate: chain.pre_certificate,
-    };
     let lookup_key = pending_entry.lookup_key();
     let signing_key = load_signing_key(env, name)?;
 
@@ -233,8 +216,7 @@ async fn add_chain_or_pre_chain(
     };
     ctlog::upload_issuers(
         &public_bucket,
-        &chain
-            .issuers
+        &req.chain[1..]
             .iter()
             .map(Vec::as_slice)
             .collect::<Vec<&[u8]>>(),

crates/ct_worker/src/lib.rs
@@ -19,11 +19,10 @@ use p256::{ecdsa::SigningKey as EcdsaSigningKey, pkcs8::DecodePrivateKey};
 use serde::Deserialize;
 use serde_bytes::ByteBuf;
 use sha2::{Digest, Sha256};
-use static_ct_api::LookupKey;
 use std::collections::{HashMap, VecDeque};
 use std::io::Write;
 use std::sync::{LazyLock, OnceLock};
-use tlog_tiles::SequenceMetadata;
+use tlog_tiles::{LookupKey, SequenceMetadata};
 use util::now_millis;
 #[allow(clippy::wildcard_imports)]
 use worker::*;

crates/ct_worker/src/sequencer_do.rs
@@ -7,15 +7,15 @@ use crate::{
     ctlog, load_public_bucket, load_signing_key, load_witness_key,
     metrics::{millis_diff_as_secs, AsF64, Metrics, ObjectMetrics},
     util::{self, now_millis},
-    DedupCache, MemoryCache, ObjectBucket, QueryParams, SequenceMetadata, BATCH_ENDPOINT, CONFIG,
-    ENTRY_ENDPOINT, METRICS_ENDPOINT, ROOTS,
+    DedupCache, LookupKey, MemoryCache, ObjectBucket, QueryParams, SequenceMetadata,
+    BATCH_ENDPOINT, CONFIG, ENTRY_ENDPOINT, METRICS_ENDPOINT, ROOTS,
 };
 use ctlog::{CreateError, LogConfig, PoolState, SequenceState};
 use futures_util::future::join_all;
 use log::{info, warn, Level};
 use static_ct_api::{
-    LogEntryTrait, LookupKey, PendingLogEntryTrait, StandardEd25519CheckpointSigner,
-    StaticCTCheckpointSigner, StaticCTLogEntry, StaticCTPendingLogEntry,
+    LogEntryTrait, PendingLogEntryTrait, StandardEd25519CheckpointSigner, StaticCTCheckpointSigner,
+    StaticCTLogEntry, StaticCTPendingLogEntry,
 };
 use std::str::FromStr;
 use std::time::Duration;

commit da8b5edcb3193f247217e45b79673ac6398d2a01

bugfix: ensure accepted root is included in chain_fingerprints
Due to a bug introduced in fb4f44d, the `certificate_chain` field of
data tiles may not include the accepted root certificate, which is a
violation of the RFC6962 requirement (inherited by static-ct-api) that
"[t]he final certificate MUST be a root certificate accepted by the
log."

This bug is triggered by add-[pre-]chain requests where the submitted
chain does not include a log-accepted root certificate. Instead of
storing the full validated chain including the accepted root
certificate, the stored `certificate_chain` only includes the
intermediate certificates submitted in the request.

The bug does not impact chain validation, so no entries were accepted
into the log that would have otherwise been accepted in the absence of
the bug.

It is also possible to recover from the bug.  As the `certificate_chain`
field of data tiles is not authenticated in the tree, it should be
possible to update data tiles to append the trusted root in cases when
it is missing.

Thanks to Andrew Ayer for spotting and reporting this bug.

crates/static_ct_api/src/rfc6962.rs
@@ -137,6 +137,11 @@ pub fn validate_chain(
         .map(|v| parse_certificate(v))
         .collect::<Result<_, _>>()?;
 
+    let mut chain_fingerprints: Vec<[u8; 32]> = raw_chain[1..]
+        .iter()
+        .map(|issuer| Sha256::digest(issuer).into())
+        .collect();
+
     // Walk up the chain, ensuring that each certificate signs the previous one.
     // This simplified chain validation is possible due to the constraints laid out in RFC 6962.
     let mut to_verify = &leaf;
@@ -172,6 +177,7 @@ pub fn validate_chain(
         }
         if is_link_valid(to_verify, &roots.certs[idx]) {
             intermediates.push(roots.certs[idx].clone());
+            chain_fingerprints.push(Sha256::digest(roots.certs[idx].to_der()?).into());
             found = true;
             break;
         }
@@ -217,11 +223,6 @@ pub fn validate_chain(
         precert_opt = None;
         certificate = leaf.to_der()?;
     }
-
-    let chain_fingerprints = raw_chain[1..]
-        .iter()
-        .map(|issuer| Sha256::digest(issuer).into())
-        .collect();
 
     Ok(StaticCTPendingLogEntry {
         certificate,
@@ -437,7 +438,7 @@ mod tests {
     );
 
     macro_rules! test_validate_chain {
-        ($name:ident; $($root_file:expr),+; $($chain_file:expr),+; $not_after_start:expr; $not_after_end:expr; $expect_precert:expr; $require_server_auth_eku:expr; $want_err:expr) => {
+        ($name:ident; $($root_file:expr),+; $($chain_file:expr),+; $not_after_start:expr; $not_after_end:expr; $expect_precert:expr; $require_server_auth_eku:expr; $want_err:expr; $want_chain_len:expr) => {
             #[test]
             fn $name() {
                 let mut roots = Vec::new();
@@ -449,28 +450,32 @@ mod tests {
                     chain.append(&mut Certificate::load_pem_chain(include_bytes!($chain_file)).unwrap());
                 )*
 
-                assert_eq!(validate_chain(
+                let result = validate_chain(
                         &x509_util::certs_to_bytes(&chain).unwrap(),
                         &CertPool::new(roots).unwrap(),
                         $not_after_start,
                         $not_after_end,
                         $expect_precert,
                         $require_server_auth_eku,
-                    )
-                    .is_err(), $want_err);
+                    );
+                assert_eq!(result.is_err(), $want_err);
+
+                if let Ok(pending_entry) = result {
+                    assert_eq!(pending_entry.chain_fingerprints.len(), $want_chain_len);
+                }
             }
         };
     }
 
     macro_rules! test_validate_chain_success {
-        ($name:ident, $($chain_file:expr),+) => {
-            test_validate_chain!($name; "../tests/fake-ca-cert.pem", "../tests/fake-root-ca-cert.pem", "../tests/ca-cert.pem", "../tests/real-precert-intermediate.pem"; $($chain_file),+; None; None; false; false; false);
+        ($name:ident, $want_chain_len:expr, $($chain_file:expr),+) => {
+            test_validate_chain!($name; "../tests/fake-ca-cert.pem", "../tests/fake-root-ca-cert.pem", "../tests/ca-cert.pem", "../tests/real-precert-intermediate.pem"; $($chain_file),+; None; None; false; false; false; $want_chain_len);
         };
     }
 
     macro_rules! test_validate_chain_fail {
         ($name:ident, $($chain_file:expr),+) => {
-            test_validate_chain!($name; "../tests/fake-ca-cert.pem", "../tests/fake-root-ca-cert.pem", "../tests/ca-cert.pem", "../tests/real-precert-intermediate.pem"; $($chain_file),+; None; None; false; false; true);
+            test_validate_chain!($name; "../tests/fake-ca-cert.pem", "../tests/fake-root-ca-cert.pem", "../tests/ca-cert.pem", "../tests/real-precert-intermediate.pem"; $($chain_file),+; None; None; false; false; true; 0);
         };
     }
 
@@ -501,31 +506,36 @@ mod tests {
     );
     test_validate_chain_success!(
         valid_chain,
+        2,
         "../tests/leaf-signed-by-fake-intermediate-cert.pem",
         "../tests/fake-intermediate-cert.pem"
     );
     test_validate_chain_success!(
         valid_chain_with_policy_constraints,
+        2,
         "../tests/leaf-cert.pem",
         "../tests/fake-intermediate-with-policy-constraints-cert.pem"
     );
     test_validate_chain_success!(
         valid_chain_with_policy_constraints_inc_root,
+        2,
         "../tests/leaf-cert.pem",
         "../tests/fake-intermediate-with-policy-constraints-cert.pem",
         "../tests/fake-root-ca-cert.pem"
     );
     test_validate_chain_success!(
         valid_chain_with_name_constraints,
+        2,
         "../tests/leaf-cert.pem",
         "../tests/fake-intermediate-with-name-constraints-cert.pem"
     );
     test_validate_chain_success!(
         valid_chain_with_invalid_name_constraints,
+        2,
         "../tests/leaf-cert.pem",
         "../tests/fake-intermediate-with-invalid-name-constraints-cert.pem"
     );
-    test_validate_chain_success!(valid_chain_of_len_4, "../tests/subleaf.chain");
+    test_validate_chain_success!(valid_chain_of_len_4, 3, "../tests/subleaf.chain");
     test_validate_chain_fail!(
         misordered_chain_of_len_4,
         "../tests/subleaf.misordered.chain"
@@ -537,16 +547,16 @@ mod tests {
 
     macro_rules! test_not_after {
         ($name:ident; $start:expr; $end:expr; $want_err:expr) => {
-            test_validate_chain!($name; "../tests/fake-ca-cert.pem"; "../tests/leaf-signed-by-fake-intermediate-cert.pem", "../tests/fake-intermediate-cert.pem"; $start; $end; false; true; $want_err);
+            test_validate_chain!($name; "../tests/fake-ca-cert.pem"; "../tests/leaf-signed-by-fake-intermediate-cert.pem", "../tests/fake-intermediate-cert.pem"; $start; $end; false; true; $want_err; 2);
         };
     }
     test_not_after!(not_after_no_range; None; None; false);
     test_not_after!(not_after_valid_range; Some(parse_datetime("2018-01-01T00:00:00Z")); Some(parse_datetime("2020-07-01T00:00:00Z")); false);
     test_not_after!(not_after_before_start; Some(parse_datetime("2020-01-01T00:00:00Z")); None; true);
     test_not_after!(not_after_after_end; None; Some(parse_datetime("1999-01-01T00:00:00Z")); true);
-    test_validate_chain!(missing_server_auth_eku_not_required; "../tests/fake-root-ca-cert.pem"; "../tests/subleaf.chain"; None; None; false; false; false);
-    test_validate_chain!(missing_server_auth_eku_required; "../tests/fake-root-ca-cert.pem"; "../tests/subleaf.chain"; None; None; false; true; true);
-    test_validate_chain!(preissuer_chain; "../tests/test-roots.pem"; "../tests/preissuer-chain.pem"; None; None; true; true; false);
+    test_validate_chain!(missing_server_auth_eku_not_required; "../tests/fake-root-ca-cert.pem"; "../tests/subleaf.chain"; None; None; false; false; false; 3);
+    test_validate_chain!(missing_server_auth_eku_required; "../tests/fake-root-ca-cert.pem"; "../tests/subleaf.chain"; None; None; false; true; true; 0);
+    test_validate_chain!(preissuer_chain; "../tests/test-roots.pem"; "../tests/preissuer-chain.pem"; None; None; true; true; false; 3);
 
     #[test]
     fn test_build_precert_tbs() {