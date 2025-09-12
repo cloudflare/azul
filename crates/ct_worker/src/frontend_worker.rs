cloudflare/azul

commit ad6ced9d2b0a7850230777b8299a47120640b330

Ensure that the inferred root is uploaded to /issuers (#93)
As reported by Andrew Ayer in
https://groups.google.com/a/chromium.org/g/ct-policy/c/dVz6tMLWsRY/m/m1_lh0IzGwAJ,
the previous bugfix was incomplete as inferred roots for chains were not
getting uploaded to /issuers.

crates/ct_worker/src/frontend_worker.rs
@@ -17,6 +17,7 @@ use static_ct_api::{AddChainRequest, GetRootsResponse, StaticCTLogEntry};
 use tlog_tiles::{LogEntry, PendingLogEntry, PendingLogEntryBlob};
 #[allow(clippy::wildcard_imports)]
 use worker::*;
+use x509_cert::der::Encode;
 
 // The Maximum Merge Delay (MMD) of a log indicates the maximum period of time
 // between when a SCT is issued and the corresponding entry is sequenced in the
@@ -174,6 +175,7 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         })
 }
 
+#[allow(clippy::too_many_lines)]
 async fn add_chain_or_pre_chain(
     mut req: Request,
     env: &Env,
@@ -190,9 +192,10 @@ async fn add_chain_or_pre_chain(
     };
 
     // Temporal interval dates prior to the Unix epoch are treated as the Unix epoch.
+    let roots = &load_roots(env, name).await?;
     let pending_entry = match static_ct_api::validate_chain(
         &req.chain,
-        &load_roots(env, name).await?,
+        roots,
         Some(
             u64::try_from(params.temporal_interval.start_inclusive.timestamp_millis())
                 .unwrap_or_default(),
@@ -232,15 +235,25 @@ async fn add_chain_or_pre_chain(
 
     // First persist issuers.
     let public_bucket = ObjectBucket::new(load_public_bucket(env, name)?);
-    generic_log_worker::upload_issuers(
-        &public_bucket,
-        &req.chain[1..]
-            .iter()
-            .map(Vec::as_slice)
-            .collect::<Vec<&[u8]>>(),
-        name,
-    )
-    .await?;
+    let mut issuers = req.chain[1..]
+        .iter()
+        .map(Vec::as_slice)
+        .collect::<Vec<&[u8]>>();
+
+    // Make sure the root is persisted as well.
+    let root_hash = pending_entry
+        .chain_fingerprints
+        .last()
+        .ok_or("chain fingerprints is empty")?;
+    let Some(root_cert) = roots.by_fingerprint(root_hash) else {
+        return Err(format!("{name}: root hash for chain is not in accepted roots").into());
+    };
+    let root_bytes = root_cert.to_der().map_err(|e| e.to_string())?;
+    if !issuers.iter().any(|issuer| *issuer == root_bytes) {
+        issuers.push(root_bytes.as_slice());
+    }
+
+    generic_log_worker::upload_issuers(&public_bucket, &issuers, name).await?;
 
     // Submit entry to be sequenced, either via a batcher or directly to the
     // sequencer.

crates/x509_util/src/lib.rs
@@ -119,4 +119,17 @@ impl CertPool {
             .by_fingerprint
             .contains_key::<[u8; 32]>(&Sha256::digest(cert.to_der()?).into()))
     }
+
+    /// Fetch a certificate from the pool by its fingerprint.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if there are issues DER-encoding the certificate.
+    pub fn by_fingerprint(&self, fingerprint: &[u8; 32]) -> Option<&Certificate> {
+        if let Some(idx) = self.by_fingerprint.get(fingerprint) {
+            self.certs.get(*idx)
+        } else {
+            None
+        }
+    }
 }

commit efe290e84056aa2a8c96ef391f45e474e6acb1ff

Keep only a single copy of roots per worker script, fixes #110
In 36bb0fc, we switched from loading roots once per Worker script (with
LazyLock) to loading roots dynamically for each fetch request, since we
need an async operation to fetch roots from KV, and LazyLock doesn't
support async initialization. This means each add-chain request has to
keep a separate copy of the roots in the Worker memoery. Combined with
ad6ced9 which caused the dynamically loaded roots to be kept in memory
across the asynchronous call to the sequencer, this resulted in the
memory issues observed in #110.

Use the runtime-agnostic tokio::sync::LazyCell to restore the previous
behavior of loading roots only once for each Worker script.

crates/ct_worker/Cargo.toml
@@ -51,6 +51,7 @@ serde_with.workspace = true
 static_ct_api.workspace = true
 signed_note.workspace = true
 tlog_tiles.workspace = true
+tokio.workspace = true
 worker.workspace = true
 x509-cert.workspace = true
 x509_util.workspace = true

crates/ct_worker/src/frontend_worker.rs
@@ -203,7 +203,7 @@ async fn add_chain_or_pre_chain(
     };
 
     // Temporal interval dates prior to the Unix epoch are treated as the Unix epoch.
-    let roots = &load_roots(env, name).await?;
+    let roots = load_roots(env, name).await?;
     let pending_entry = match static_ct_api::partially_validate_chain(
         &req.chain,
         roots,

crates/ct_worker/src/lib.rs
@@ -11,6 +11,7 @@ use static_ct_api::StaticCTCheckpointSigner;
 use std::collections::HashMap;
 use std::sync::{LazyLock, OnceLock};
 use tlog_tiles::{CheckpointSigner, Ed25519CheckpointSigner, SequenceMetadata};
+use tokio::sync::OnceCell;
 use worker::{Env, Result};
 use x509_util::CertPool;
 
@@ -34,6 +35,7 @@ static CONFIG: LazyLock<AppConfig> = LazyLock::new(|| {
 
 static SIGNING_KEY_MAP: OnceLock<HashMap<String, OnceLock<EcdsaSigningKey>>> = OnceLock::new();
 static WITNESS_KEY_MAP: OnceLock<HashMap<String, OnceLock<Ed25519SigningKey>>> = OnceLock::new();
+static ROOTS: OnceCell<CertPool> = OnceCell::const_new();
 
 pub(crate) fn load_signing_key(env: &Env, name: &str) -> Result<&'static EcdsaSigningKey> {
     let once = &SIGNING_KEY_MAP.get_or_init(|| {
@@ -107,33 +109,37 @@ fn ccadb_roots_filename(name: &str) -> String {
     format!("roots_{name}.pem")
 }
 
-async fn load_roots(env: &Env, name: &str) -> Result<CertPool> {
+async fn load_roots(env: &Env, name: &str) -> Result<&'static CertPool> {
     // Load embedded roots.
-    let pem = include_bytes!(concat!(env!("OUT_DIR"), "/roots.pem"));
-    let mut pool = CertPool::default();
-    // load_pem_chain fails on empty input: https://github.com/RustCrypto/formats/pull/1965
-    if !pem.is_empty() {
-        pool.append_certs_from_pem(pem)
-            .map_err(|e| format!("failed to load PEM chain: {e}"))?;
-    }
+    ROOTS
+        .get_or_try_init(|| async {
+            let pem = include_bytes!(concat!(env!("OUT_DIR"), "/roots.pem"));
+            let mut pool = CertPool::default();
+            // load_pem_chain fails on empty input: https://github.com/RustCrypto/formats/pull/1965
+            if !pem.is_empty() {
+                pool.append_certs_from_pem(pem)
+                    .map_err(|e| format!("failed to load PEM chain: {e}"))?;
+            }
 
-    // Load additional roots from the CCADB roots file in Workers KV.
-    if CONFIG.logs[name].enable_ccadb_roots {
-        let key = ccadb_roots_filename(name);
-        let kv = env.kv(CCADB_ROOTS_NAMESPACE)?;
-        let pem = if let Some(pem) = kv.get(&key).text().await? {
-            pem
-        } else {
-            // The roots file might not exist if the CCADB roots cron job hasn't
-            // run yet. Try to create it once before failing.
-            update_ccadb_roots(&[&key], &kv).await?;
-            kv.get(&key)
-                .text()
-                .await?
-                .ok_or(format!("{name}: '{key}' not found in KV"))?
-        };
-        pool.append_certs_from_pem(pem.as_bytes())
-            .map_err(|e| format!("failed to add CCADB certs to pool: {e}"))?;
-    }
-    Ok(pool)
+            // Load additional roots from the CCADB roots file in Workers KV.
+            if CONFIG.logs[name].enable_ccadb_roots {
+                let key = ccadb_roots_filename(name);
+                let kv = env.kv(CCADB_ROOTS_NAMESPACE)?;
+                let pem = if let Some(pem) = kv.get(&key).text().await? {
+                    pem
+                } else {
+                    // The roots file might not exist if the CCADB roots cron job hasn't
+                    // run yet. Try to create it once before failing.
+                    update_ccadb_roots(&[&key], &kv).await?;
+                    kv.get(&key)
+                        .text()
+                        .await?
+                        .ok_or(format!("{name}: '{key}' not found in KV"))?
+                };
+                pool.append_certs_from_pem(pem.as_bytes())
+                    .map_err(|e| format!("failed to add CCADB certs to pool: {e}"))?;
+            }
+            Ok(pool)
+        })
+        .await
 }