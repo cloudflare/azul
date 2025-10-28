bd3496a161e17ebe11a7ae95cff2bf3e457226f1
Christopher Patton <cpatton@cloudflare.com>
2025-10-28 07:06:09 -0700

    Implement `Display` for `RelativeOid`
    For the purposes of computing the tlog key ID, the log ID is used as the
    key name. Add a method for encoding the arcs as a string so that we can
    properly construct the key name.
    
    Implementation note: Currently we only keep around the DER encoding of
    the arcs. We could decode the arcs from the DER blob, but it's a bit
    more convenient to just keep the arcs around in the data structure.

--- crates/mtc_api/src/relative_oid.rs
+++ crates/mtc_api/src/relative_oid.rs
@@ -7,6 +7,7 @@ use std::str::FromStr;
 #[derive(Clone)]
 pub struct RelativeOid {
     ber: Vec<u8>,
+    arcs: Vec<u32>,
 }
 
 impl RelativeOid {
@@ -30,12 +31,24 @@ impl RelativeOid {
         if ber.len() > 255 {
             return Err(MtcError::Dynamic("invalid relative OID".into()));
         }
-        Ok(Self { ber })
+        Ok(Self {
+            ber,
+            arcs: arcs.to_vec(),
+        })
     }
 
     /// Returns the DER-encoded content bytes.
     pub fn as_bytes(&self) -> &[u8] {
         &self.ber
+    }
+}
+
+impl std::fmt::Display for RelativeOid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for arc in self.arcs.iter().take(self.arcs.len() - 1) {
+            write!(f, "{}.", arc)?;
+        }
+        write!(f, "{}", self.arcs[self.arcs.len() - 1])
     }
 }
 
@@ -61,9 +74,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn encode_decode() {
+    fn encode_tagged() {
         let relative_oid = RelativeOid::from_str("13335.2").unwrap();
         let any = Any::new(Tag::RelativeOid, relative_oid.as_bytes()).unwrap();
         assert_eq!(any.to_der().unwrap(), b"\x0d\x03\xe8\x17\x02");
+    }
+
+    #[test]
+    fn encode_string() {
+        let relative_oid = RelativeOid::from_str("13335.2").unwrap();
+        assert_eq!(relative_oid.to_string(), "13335.2");
+    }
+
+    #[test]
+    fn decode_string_encode_bytes() {
+        struct TestCase {
+            s: &'static str,
+            b: &'static [u8],
+        }
+        for TestCase { s, b } in [
+            TestCase {
+                s: "237",
+                b: &[129, 109],
+            },
+            TestCase {
+                s: "1.2.3.4",
+                b: &[1, 2, 3, 4],
+            },
+            TestCase {
+                s: "13335.2",
+                b: &[232, 23, 2],
+            },
+            TestCase {
+                s: "44363.48.10",
+                b: &[130, 218, 75, 48, 10],
+            },
+        ] {
+            let relative_oid = RelativeOid::from_str(s).unwrap();
+            assert_eq!(relative_oid.as_bytes(), b);
+        }
     }
 }
