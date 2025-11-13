1d8da0c756ea0fed18568e886fdd2ccdeca49463
Michael Rosenberg <mrosenberg@cloudflare.com>
2025-11-13 15:46:11 -0500

    Add docs to testing script

--- crates/ct_worker/scripts/add_cert_to_local_dev.py
+++ crates/ct_worker/scripts/add_cert_to_local_dev.py
@@ -8,28 +8,32 @@ import sys
 
 HOST = "www.google.com"
 
+# Need >=3.13 to use conn.get_unverified_chain()
 if sys.version_info < (3, 13, 0):
     cur_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
     print("Error: This script requires Python 3.13 or newer. ")
     print(f"Current version is {cur_version}")
     exit(1)
 
+# Make a connection to HOST
 context = ssl.create_default_context()
 conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=HOST)
-
 conn.connect((HOST, 443))
-parsed_cert = conn.getpeercert()
 
+# Get the leaf cert. That's the not_after we use to decide which shard to submit to
+parsed_cert = conn.getpeercert()
+# Get the cert chain. This is what we'll submit
 der_cert_chain = conn.get_unverified_chain()
 b64_cert_chain = [str(base64.b64encode(c), encoding="utf-8") for c in der_cert_chain]
 
+# Figure out the year and half-year that this not_after belongs to. Eg dev2026h2a is the second half
+# of 2026
 not_after = datetime.strptime(parsed_cert["notAfter"], "%b %d %H:%M:%S %Y %Z")
 not_after_year = not_after.year
 not_after_half = 1 if not_after.month < 7 else 2
-
 log_name = f"dev{not_after_year}h{not_after_half}a"
 
-# Make a post request
+# Make a POST request to localhost add-chain
 payload = {"chain": b64_cert_chain}
 request = urllib.request.Request(
     f"http://localhost:8787/logs/{log_name}/ct/v1/add-chain",
