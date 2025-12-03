7de093ec6b506a17b4af215349ef17b257e99d3d
Mendes <pedro.mendes.26@gmail.com>
2025-12-03 10:32:11 +0000

    Improve utility scripts (#169)
    * Make the create log script edit the wrangler.jsonc
    
    * Add script to create the ccadb_roots kv namespace
    
    * Make location optional
    
    * Improve script correctness
    
    * Update ct_worker/readme to mention scripts
    
    * Run shellcheck

--- crates/ct_worker/scripts/create-log.sh
+++ crates/ct_worker/scripts/create-log.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 set -e -o pipefail
+cd "$(dirname "$0")/.." || exit # this script assumes it's runnnig inside the ct_worker dir
 
 # Helper script to create resources for a log shard.
 
-if [ -z $ENV ] || [ -z $LOG_NAME ] || [ -z $LOCATION ] || [ -z $CLOUDFLARE_ACCOUNT_ID ]; then
-	echo "ENV, LOG_NAME, LOCATION, and CLOUDFLARE_ACCOUNT_ID must all be set"
+if [ -z "${ENV}" ] || [ -z "${LOG_NAME}" ] || [ -z "${CLOUDFLARE_ACCOUNT_ID}" ]; then
+	echo "ENV, LOG_NAME, and CLOUDFLARE_ACCOUNT_ID must all be set"
 	exit 1
 fi
 
 WRANGLER_CONF=${WRANGLER_CONF:-wrangler.jsonc}
 
 while true; do
-    read -p "Do you want to proceed with ENV=${ENV}, LOG_NAME=${LOG_NAME}, LOCATION=${LOCATION}, CLOUDFLARE_ACCOUNT_ID=${CLOUDFLARE_ACCOUNT_ID}? (y/N) " yn
+    if [ "${LOCATION}" ]; then
+        L=", LOCATION=${LOCATION}"
+    fi
+    read -rp "Do you want to proceed with ENV=${ENV}, LOG_NAME=${LOG_NAME}${L}, CLOUDFLARE_ACCOUNT_ID=${CLOUDFLARE_ACCOUNT_ID}? (y/N) " yn
     case $yn in
         [yY] ) echo "Proceeding..."; break;;
         [nN] ) echo "Exiting..."; exit;;
@@ -20,30 +24,51 @@ while true; do
     esac
 done
 
+
+# https://github.com/cloudflare/azul/pull/169#discussion_r2582145507
+location=()
+if [ "${LOCATION}" ]; then
+    location=(--location "${LOCATION}")
+fi
+
 # Create R2 bucket if it does not already exist
-npx wrangler -e="${ENV}" -c "${WRANGLER_CONF}" r2 bucket create static-ct-public-${LOG_NAME} --location ${LOCATION}
+npx wrangler \
+    -e="${ENV}" \
+    -c "${WRANGLER_CONF}" \
+    r2 bucket create \
+    "static-ct-public-${LOG_NAME}" \
+    --update-config \
+    --binding "public_${LOG_NAME}" "${location[@]}"
 
 # Create KV namespace if it does not already exist
-npx wrangler -e="${ENV}" -c "${WRANGLER_CONF}" kv namespace create static-ct-cache-${LOG_NAME}
+npx wrangler \
+    -e="${ENV}" \
+    -c "${WRANGLER_CONF}" \
+    kv namespace create \
+    "static-ct-cache-${LOG_NAME}" \
+    --update-config \
+    --binding "cache_${LOG_NAME}"
 
 # Create witness and log signing keys if they do not already exist
-if npx wrangler -e=${ENV} secret list | grep -q WITNESS_KEY_${LOG_NAME}; then
+if npx wrangler -e="${ENV}" -c "${WRANGLER_CONF}" secret list | grep -q "WITNESS_KEY_${LOG_NAME}"; then
 	echo "WITNESS_KEY_${LOG_NAME} already exists"
 else
-	openssl genpkey -algorithm ed25519 | npx wrangler -c "$WRANGLER_CONF" -e=${ENV} secret put WITNESS_KEY_${LOG_NAME}
+	openssl genpkey -algorithm ed25519 |
+        npx wrangler -e="${ENV}" -c "${WRANGLER_CONF}" secret put "WITNESS_KEY_${LOG_NAME}"
 fi
-if npx wrangler -e=${ENV} secret list | grep -q SIGNING_KEY_${LOG_NAME}; then
+if npx wrangler -e="${ENV}" -c "${WRANGLER_CONF}" secret list | grep -q "SIGNING_KEY_${LOG_NAME}"; then
 	echo "SIGNING_KEY_${LOG_NAME} already exists"
 else
-	openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 | npx wrangler -c "$WRANGLER_CONF" -e=${ENV} secret put SIGNING_KEY_${LOG_NAME}
+	openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 |
+        npx wrangler -e="${ENV}" -c "${WRANGLER_CONF}" secret put "SIGNING_KEY_${LOG_NAME}"
 fi
 
 echo "DONE"
 echo "NOTE: If you intend to run wrangler dev with this log, you must add the appropriate signing keys to .dev.vars"
 echo "~~~~~~"
-echo "echo -n \"SIGNING_KEY_${LOG_NAME}=\\\\\"\" >> .dev.vars"
-echo "openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 | sed 's/$/\\\\\\\\\\\\\\\\n/g' | tr -d \\\\n >> .dev.vars"
-echo "echo '\"' >> .dev.vars"
-echo "echo -n \"WITNESS_KEY_${LOG_NAME}=\\\\\"\" >> .dev.vars"
-echo "openssl genpkey -algorithm ed25519 | sed 's/$/\\\\\\\\\\\\\\\\n/g' | tr -d \\\\n >> .dev.vars"
-echo "echo '\"' >> .dev.vars"
+printf 'echo -n "SIGNING_KEY_%s=\\"" >> .dev.vars\n' "${LOG_NAME}"
+printf 'openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 | sed '\''s/$/\\\\n/g'\'' | tr -d '\''\\n'\'' >> .dev.vars\n'
+printf 'echo \\" >> .dev.vars\n'
+printf 'echo -n "WITNESS_KEY_%s=\\"" >> .dev.vars\n' "${LOG_NAME}"
+printf 'openssl genpkey -algorithm ed25519 | sed '\''s/$/\\\\n/g'\'' | tr -d '\''\\n'\'' >> .dev.vars\n'
+printf 'echo \\" >> .dev.vars\n'
---
+++ crates/ct_worker/scripts/create-root-kv.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+cd "$(dirname "$0")/.." || exit # this script assumes it's runnnig inside the ct_worker dir
+
+# Helper script to create resources for a log shard.
+
+if [ -z "${ENV}" ] || [ -z "${CLOUDFLARE_ACCOUNT_ID}" ]; then
+	echo "ENV and CLOUDFLARE_ACCOUNT_ID must all be set"
+	exit 1
+fi
+
+WRANGLER_CONF=${WRANGLER_CONF:-wrangler.jsonc}
+
+while true; do
+    read -rp "Do you want to proceed with ENV=${ENV}, CLOUDFLARE_ACCOUNT_ID=${CLOUDFLARE_ACCOUNT_ID}? (y/N) " yn
+    case $yn in
+        [yY] ) echo "Proceeding..."; break;;
+        [nN] ) echo "Exiting..."; exit;;
+        * ) echo "Invalid input. Please enter 'y' or 'N'.";;
+    esac
+done
+
+# Create KV namespace if it does not already exist
+npx wrangler \
+    -e="${ENV}" \
+    -c "${WRANGLER_CONF}" \
+    kv namespace create \
+    static-ct-ccadb-roots \
+    --update-config \
+    --binding ccadb_roots
